8d90795d81d44257c143b91b6a9ace155c84bf00
ajfrierson <alvinfrierson@fuller.edu>
2019-01-16 15:39:30 -0800

    added final authentication so that page needs login information

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage';
-
+import Authenticate from './components/Authentication/Authenticate';
 
 class App extends Component {
   constructor() {
@@ -18,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Authenticate(App);
